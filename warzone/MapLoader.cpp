c7cc96085c3ce23b44ee009e2096d4018d5bfbe5
Maël Semler <leamsem719@hotmail.ca>
2020-10-01 14:58:21 -0400

    Added MapLoaderDriver file and constructors in MapLoader

--- warzone/MapLoader.cpp
+++ warzone/MapLoader.cpp
@@ -1,2 +1,22 @@
 #include "MapLoader.h"
+#include <fstream>
+#include <iostream>
 
+
+
+MapLoader::MapLoader() {
+	this->fileName = NULL;
+}
+
+MapLoader::MapLoader(std::string fileName) {
+	this->fileName = new std::string(fileName);
+}
+
+MapLoader::~MapLoader() {
+	delete	fileName;
+}
+
+
+bool MapLoader::validateMap() {
+
+}
--- warzone/MapLoader.h
+++ warzone/MapLoader.h
@@ -1,11 +1,18 @@
 #pragma once
-
-
+#include "Map.h"
+#include <string>
+#include <vector>
 
 class MapLoader {
 public:
-
+	MapLoader();
+	MapLoader(std::string fileName);
+	~MapLoader();
+	bool validateMap();
 
 private:
-
+	std::string* fileName;
+	bool checkContinents();
+	bool checkCountries();
+	bool checkBorders();
 };
