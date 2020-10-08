78e54e0206a47a3e1860cbda62274d484f778a36
yabdelkawy <yabdelkawy@outlook.com>
2020-10-08 13:33:27 -0400

    Created header files with appropriate function declarations

--- warzone/Player.h
+++ warzone/Player.h
@@ -1,5 +1,28 @@
 #pragma once
 
-class Player {
+using namespace std;
 
+#include<vector>
+#include<iostream>
+
+
+class Player {
+public:
+	Player(); //constructor 
+	Player(Territory* initialTerritories);  //constructor
+	~Player();   //destructor
+	Player& operator= (const Player* one);  //assignment operator
+	Player(const Player& player);  //copy constructor 
+
+	friend ostream& operator<< (ostream& output, const Player &player); //stream overloading
+	friend istream& operator>> (istream& input, Player &player);   //stream overloading
+
+	vector<Territory*> toDefend(); //Returns territories to defend
+	vector<Territory*> toAttack(); //Returns territories to attack
+	//void issueOrder();
+	
+private:
+	vector<Territory*>* playerTerritory; //returns player's territories
+//	vector<Order*>* playerOrders; //returns player's orders
+//	Hand* playerHand; //returns player's hand
 };
