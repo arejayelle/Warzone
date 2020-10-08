cdc2c8749fc2d38016ba9b1bd7a8487794bd8dbf
yabdelkawy <yabdelkawy@outlook.com>
2020-10-08 14:38:08 -0400

    Created definitions for player.cpp

--- warzone/Player.cpp
+++ warzone/Player.cpp
@@ -1,6 +1,75 @@
 #include "Player.h"
 
 Player::Player() {
-	this-> playerTerritory = 0;
-	this-> 
+	this->playerTerritory = new vector<Territory*>();
+	this->playerOrders = new vector<Orders*>();
+	this->playerHand = 0;
+}
+
+Player::Player(vector<Territory*>* territoriesToAdd, vector<Orders*>* ordersToAdd, Hand* handToAdd) {
+	this->playerTerritory = territoriesToAdd;
+	this->playerOrders = ordersToAdd;
+	this->playerHand = handToAdd;
+}
+
+Player::~Player() {
+	delete playerTerritory;
+	delete playerOrders;
+	delete playerHand;
+}
+
+Player& Player::operator=(const Player* one)
+{
+	
+}
+
+Player::Player(const Player& player)
+{
+}
+
+vector<Territory*> Player::toDefend()
+{
+	return vector<Territory*>();
+}
+
+vector<Territory*> Player::toAttack()
+{
+	
+}
+
+vector<Territory*>* Player::getTerritories()
+{
+	return nullptr;
+}
+
+vector<Orders*>* Player::getOrders()
+{
+	return nullptr;
+}
+
+Hand* Player::getHand()
+{
+	return nullptr;
+}
+
+void Player::setTerritories(vector<Territory*>* territoriesToAdd)
+{
+}
+
+void Player::setOrders(vector<Orders*>* ordersToAdd)
+{
+}
+
+void Player::setHand(Hand* handToAdd)
+{
+}
+
+ostream& operator<<(ostream& output, const Player& player)
+{
+
+}
+
+istream& operator>>(istream& input, Player& player)
+{
+
 }
--- warzone/Player.h
+++ warzone/Player.h
@@ -3,7 +3,7 @@
 using namespace std;
 #include "Map.h"
 #include "Hand.h"
-#include "Order.h"
+#include "Orders.h"
 #include<vector>
 #include<iostream>
 
@@ -11,7 +11,7 @@ using namespace std;
 class Player {
 public:
 	Player(); //constructor 
-	Player(vector<Territory> territoriesToAdd);  //constructor
+	Player(vector<Territory*>* territoriesToAdd, vector<Orders*>* ordersToAdd, Hand* handToAdd);  //constructor
 	~Player();   //destructor
 	Player& operator= (const Player* one);  //assignment operator
 	Player(const Player& player);  //copy constructor 
@@ -22,9 +22,17 @@ public:
 	vector<Territory*> toDefend(); //Returns territories to defend
 	vector<Territory*> toAttack(); //Returns territories to attack
 	//void issueOrder();
+
+	vector<Territory*>* getTerritories(); //get territories
+	vector<Orders*>* getOrders(); //get orders
+	Hand* getHand(); //get hand
+
+	void setTerritories(vector<Territory*>* territoriesToAdd);  //set territories 
+	void setOrders(vector<Orders*>* ordersToAdd); //set orders
+	void setHand(Hand* handToAdd);  //set hand
 	
 private:
 	vector<Territory*>* playerTerritory; //returns player's territories
-	vector<Order*>* playerOrders; //returns player's orders
+	vector<Orders*>* playerOrders; //returns player's orders
 	Hand* playerHand; //returns player's hand
 };
