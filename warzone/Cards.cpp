38d8598a572f671839358785a884824a3f8b1fa3
Jun Loke <rebecca.j.loke@gmail.com>
2020-10-08 23:03:06 -0400

    Finish adding comments

--- warzone/Cards.cpp
+++ warzone/Cards.cpp
@@ -25,6 +25,8 @@ namespace Cards {
 	/**
 	 * Card Destructor
 	 * 
+	* Does nothing for now. Does not have any attributes not owned by the parent Card Class
+	 * 
 	 */
 	Card::~Card()
 	{
@@ -95,6 +97,8 @@ namespace Cards {
 	/**
 	 * Spy Card Destructor
 	 * 
+	 * Does nothing for now. Does not have any attributes not owned by the parent Card Class
+	 * 
 	 */
 	SpyCard::~SpyCard()
 	{
@@ -121,19 +125,40 @@ namespace Cards {
 	}
 
 	// Bomb Cards
+	/**
+	 * BombCard Default Constructor
+	 * 
+	 */
 	BombCard::BombCard(): Card()
 	{
 		this->name = new std::string("BombCard");
 	}
 
+	/**
+	 * BombCard Copy constructor
+	 * 
+	 * \param bombCard
+	 */
 	BombCard::BombCard(BombCard* bombCard) : Card(bombCard)
 	{
 	}
 
+	/**
+	 * BombCard Destructor
+	 * 
+	 * Does nothing for now. Does not have any attributes not owned by the parent Card Class
+	 * 
+	 */
 	BombCard::~BombCard()
 	{
 	}
 
+	/**
+	 * BombCard Assignment operator makes a copy of the passed Bomb card
+	 * 
+	 * \param card card to be copied
+	 * \return 
+	 */
 	BombCard& BombCard::operator=(const BombCard& card)
 	{
 		return new BombCard(card);
@@ -149,19 +174,37 @@ namespace Cards {
 	}
 
 	// Reinforcement Cards
+	/**
+	 * ReinforcementCard Default Constructor
+	 * 
+	 */
 	ReinforcementCard::ReinforcementCard() : Card()
 	{
 		name = new std::string("ReinforcementCard");
 	}
 
+	/**
+	 * ReinforcementCard Copy Constructor
+	 * 
+	 * \param rCard
+	 */
 	ReinforcementCard::ReinforcementCard(ReinforcementCard* rCard) : Card(rCard)
 	{
 	}
 
+	/**
+	 * ReinforcementCard destructor
+	 * 
+	 * Does nothing for now. Does not have any attributes not owned by the parent Card Class
+	 * 
+	 */
 	ReinforcementCard::~ReinforcementCard()
 	{
 	}
 
+	/**
+	 * Assignmenet operator makes copy of the passed card
+	 */
 	ReinforcementCard& ReinforcementCard::operator=(const ReinforcementCard& card)
 	{
 		return new ReinforcementCard(card);
@@ -177,21 +220,40 @@ namespace Cards {
 	}
 
 	// Blockade Cards
+	/**
+	 * BlockadeCard Default Constructor
+	 * 
+	 */
 	BlockadeCard::BlockadeCard(): Card()
 	{
 		name = new std::string("BlockadeCard");
 	}
 
-	
+	/**
+	 * BlockadeCard Copy Constructor
+	 * 
+	 * \param bCard
+	 */
 	BlockadeCard::BlockadeCard(BlockadeCard* bCard) : Card(bCard)
 	{
 	}
 
+	/**
+	 * Makes a copy of the passed card
+	 * 
+	 * \param card
+	 * \return 
+	 */
 	BlockadeCard& BlockadeCard::operator=(const BlockadeCard& card)
 	{
 		return new BlockadeCard(card);
 	}
 
+	/**
+	 * 
+	 * Does nothing for now. Does not have any attributes not owned by the parent Card Class
+	 *  
+	 */
 	BlockadeCard::~BlockadeCard()
 	{
 
@@ -207,20 +269,40 @@ namespace Cards {
 	}
 
 	// Airlift Cards
+	/**
+	 * Default constructor
+	 * 
+	 */
 	AirliftCard::AirliftCard() : Card()
 	{
 		name = new std::string("AirliftCard");
 	}
 
+	/**
+	 * Copy Constructor.
+	 * 
+	 * \param aCard
+	 */
 	AirliftCard::AirliftCard(AirliftCard* aCard) : Card(aCard)
 	{
 	}
 
+	/**
+	 * 
+	 * Does nothing for now. Does not have any attributes not owned by the parent Card Class
+	 *  
+	 */
 	AirliftCard::~AirliftCard()
 	{
 
 	}
 
+	/**
+	 * Makes a copy and assigns it
+	 * 
+	 * \param card
+	 * \return 
+	 */
 	AirliftCard& AirliftCard::operator=(const AirliftCard& card)
 	{
 		return new AirliftCard(card);
@@ -236,20 +318,39 @@ namespace Cards {
 	}
 
 	// Diplomacy Cards
+	/**
+	 * Default constructor
+	 * 
+	 */
 	DiplomacyCard::DiplomacyCard() : Card()
 	{
 		name = new std::string("DiplomacyCard");
 	}
 
+	/**
+	 * Copy Constructor
+	 * 
+	 * \param dCard
+	 */
 	DiplomacyCard::DiplomacyCard(DiplomacyCard* dCard) : Card(dCard)
 	{
 	}
 
+	/**
+	 * Does nothing for now. Does not have any attributes not owned by the parent Card Class
+	 * 
+	 */
 	DiplomacyCard::~DiplomacyCard()
 	{
 
 	}
 
+	/**
+	 * Makes a copy and assigns it
+	 * 
+	 * \param card
+	 * \return 
+	 */
 	DiplomacyCard& DiplomacyCard::operator=(const DiplomacyCard& card)
 	{
 		return new DiplomacyCard(card);
@@ -265,6 +366,11 @@ namespace Cards {
 	}
 
 	// Deck functions
+
+	/**
+	 * Default constructor
+	 * 
+	 */
 	Deck::Deck()
 	{
 		this->fullDeck = new std::vector<Card*>;
@@ -272,6 +378,11 @@ namespace Cards {
 		return;
 	}
 
+	/**
+	 * Copy constructor
+	 * 
+	 * \param deck
+	 */
 	Deck::Deck(Deck* deck)
 	{
 		this->fullDeck = new vector<Card*>;
@@ -287,6 +398,10 @@ namespace Cards {
 		}
 	}
 
+	/**
+	 * Does nothing for now. Does not have any attributes not owned by the parent Card Class
+	 *
+	 */
 	Deck::~Deck()
 	{
 		fullDeck->clear();
@@ -295,11 +410,22 @@ namespace Cards {
 		delete drawPile;
 	}
 
+	/**
+	 * Makes a copy of the passed deck and assigns it
+	 * 
+	 * \param deck
+	 * \return 
+	 */
 	Deck& Deck::operator=(const Deck& deck)
 	{
 		return new Deck(deck);
 	}
 
+	/**
+	 * Adds the passed card to the deck.
+	 * 
+	 * \param card
+	 */
 	void Deck::add(Card* card)
 	{
 		fullDeck->push_back(card);
@@ -308,6 +434,12 @@ namespace Cards {
 
 	}
 
+	/**
+	 * Returns a card based on its ID
+	 * 
+	 * \param cardID
+	 * \return 
+	 */
 	Card* Deck::getFromCatalog(int* cardID)
 	{
 		if ((size_t)cardID < fullDeck->size())
@@ -317,6 +449,12 @@ namespace Cards {
 		return nullptr;
 	}
 
+	/**
+	 * Randomly chooses a card from the drawpile and retrieves the stored ID
+	 * removes value from the draw pile
+	 * 
+	 * \return cardID of drawn card
+	 */
 	int* Deck::draw()
 	{
 		// randomly choose a value from the draw pile
@@ -333,14 +471,23 @@ namespace Cards {
 		return cardID;
 	}
 
-	/*
-	* Takes cardID and adds it to the bottom of the drawpile
-	*/
+	/**
+	 * Takes cardID and adds it to the bottom of the drawpile
+	 * 
+	 * \param cardID
+	 */
 	void Deck::returnToDrawPile(int* cardID)
 	{
 		drawPile->push_back(cardID);
 	}
 
+	/**
+	 * print deck
+	 * 
+	 * \param out
+	 * \param deck
+	 * \return 
+	 */
 	ostream& operator<<(ostream& out, const Deck& deck)
 	{
 		out << "Here are the contents of the deck" << endl;
@@ -355,6 +502,10 @@ namespace Cards {
 		return out;
 	}
 	
+	/**
+	 * Print drawpile for debug reasons
+	 * 
+	 */
 	void Deck::printDrawpile()
 	{
 		cout << "Here are the contents of the drawpile" << endl;
@@ -369,12 +520,22 @@ namespace Cards {
 	}
 
 	// Hand functions
+	/**
+	 * Constructor with reference to the Deck
+	 * 
+	 * \param deck
+	 */
 	Hand::Hand(Deck* deck)
 	{
 		this->deck = deck;
 		this->currentHand = new vector<int*>;
 	}
 
+	/**
+	 * Copy constructor
+	 * 
+	 * \param hand
+	 */
 	Hand::Hand(Hand* hand)
 	{
 		this->deck = new Deck(hand->deck);
@@ -386,6 +547,10 @@ namespace Cards {
 		}
 	}
 
+	/**
+	 * Destructor
+	 * 
+	 */
 	Hand::~Hand()
 	{
 		delete deck;
@@ -393,11 +558,22 @@ namespace Cards {
 		delete  currentHand;
 	}
 
+	/**
+	 * Makes a copy and assigns it
+	 * 
+	 * \param hand
+	 * \return 
+	 */
 	Hand& Hand::operator=(const Hand& hand)
 	{
 		return new Hand(hand);
 	}
 
+	/**
+	 * Adds a cardID to the hand
+	 * 
+	 * \param cardId
+	 */
 	void Hand::addCard(int* cardId)
 	{
 		currentHand->push_back(cardId);
@@ -419,10 +595,22 @@ namespace Cards {
 			currentHand->erase(it);
 		}
 	}
+
+	/**
+	 * \return currentHand
+	 */
 	vector<int*>* Hand::getCurrentHand()
 	{
 		return this->currentHand;
 	}
+
+	/**
+	 * Stream operator that prints the hand contents
+	 * 
+	 * \param out
+	 * \param hand
+	 * \return 
+	 */
 	ostream& operator<<(ostream& out, const Hand& hand)
 	{
 		out << "This is my current hand" << endl;
