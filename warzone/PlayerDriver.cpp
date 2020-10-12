8a5eb8e4363b697198daafc894c0ffce5b5ca32f
Jun Loke <rebecca.j.loke@gmail.com>
2020-10-12 14:05:47 -0400

    use proper constructor for the Player driver and fix indentation

--- warzone/PlayerDriver.cpp
+++ warzone/PlayerDriver.cpp
@@ -2,20 +2,23 @@
 
 
 int PlayerDriver::main() {
-	Player* p1 = new Player();
+	vector<Territory*>* territories = new vector<Territory*>;
+	OrdersList* ordersList = new OrdersList();
+
+	Player* p1 = new Player(territories, ordersList, nullptr);
 
 	cout << "Player can defend: " << endl;  //returns territories the player can defend
-	cout<<p1->toDefend()<< endl; //prints memory address of territory
+	cout << p1->toDefend() << endl; //prints memory address of territory
 
 	cout << "Player can attack: " << endl; //returns territories the player can attack
-	cout<<p1->toAttack() << endl;
+	cout << p1->toAttack() << endl;
 
 	cout << "Player has following orders: " << endl;  //returns the player's order list
 	p1->getOrdersList()->print();
 
 	cout << "Player deployed following order: ";
 	Order* playerOrder = new Order();
-	cout << playerOrder->toString()<<endl;
+	cout << playerOrder->toString() << endl;
 	p1->issueOrder(playerOrder);  //issuing an order
 
 	p1->getOrdersList()->print();
