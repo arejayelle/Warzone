e8acdd250f210b031f66000f6d888abca6a08076
Felix Lapierre <felix.lapierre1@gmail.com>
2020-11-18 20:56:40 -0500

    Added one map and fixed some bugs

--- warzone/Main.cpp
+++ warzone/Main.cpp
@@ -6,7 +6,10 @@ int main()
 	srand((unsigned int)time(NULL));
 
 	return OrdersDriver::main();
-	//return GameEngineDriver::main();
-	//return GameObserversDriver::main();
-	//return GameLoopDriver::main();
+	// return GameEngineDriver::main();
+	// return GameObserversDriver::main();
+	// return GameLoopDriver::main();
+	GameEngine* engine = new GameEngine();
+	engine->startUpPhase();
+	engine->mainGameLoop();
 }
