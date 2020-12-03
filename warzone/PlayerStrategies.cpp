bcb7f36e41329806461d3a85107265fee86617d1
Yomna Abdelkawy <44946172+yabdelkawy@users.noreply.github.com>
2020-12-02 21:04:35 -0500

    Fixed order of method

--- warzone/PlayerStrategies.cpp
+++ warzone/PlayerStrategies.cpp
@@ -357,14 +357,14 @@ bool AggressiveComputerStrategy::issueOrder(Player* player)
 	return false;
 }
 
-bool compareTerritoriesArmiesAscendingOrder(Territory* i, Territory* j)
+bool compareTerritoriesArmiesDescendingOrder(Territory* i, Territory* j)
 {
 	return i->getArmies() > j->getArmies();
 }
 
 const vector<Territory*>* AggressiveComputerStrategy::toDefend(Player* player)
 {
-	std::sort(player->getTerritories()->begin(), player->getTerritories()->end(), compareTerritoriesArmiesAscendingOrder);
+	std::sort(player->getTerritories()->begin(), player->getTerritories()->end(), compareTerritoriesArmiesDescendingOrder);
 	return player->getTerritories();
 
 }
